5cc7c574778d2502f1df1404070f412b5595743f
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-16 00:08:53 +0300

    Adding some hover styles for markers

--- src/marker.js
+++ src/marker.js
@@ -7,12 +7,16 @@ const Marker = styled.div`
   background: red;
 
   // /* The location of the middle of the marker should be where the user clicked */
-  // top: ${props => props.y - MARKER_SIZE / 2}px;
+  // top: -${MARKER_SIZE / 2}px;
   // left: ${props => props.x - MARKER_SIZE / 2}px;
   position: relative;
   width: ${MARKER_SIZE}px;
   height: ${MARKER_SIZE}px;
-  cursor: pointer;
+
+  &:hover {
+    border: 2px solid blue;
+    cursor: pointer;
+  }
 `
 
 export default Marker
