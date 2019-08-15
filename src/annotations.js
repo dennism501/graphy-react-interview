b4f10898455d46c2e7d65da0c76fd3d6abc3c5bc
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-15 22:34:31 +0300

    Starting to add open/closing a tooltip

--- src/annotations.js
+++ src/annotations.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { findIndex, get, find, uniqueId } from 'lodash'
 
 import Marker from './marker'
 import Tooltip from './tooltip'
@@ -23,6 +24,7 @@ class Annotations extends React.Component {
     }
 
     this.handleSelection = this.handleSelection.bind(this)
+    this.handleMarkerClick = this.handleMarkerClick.bind(this)
   }
 
   handleSelection(ev) {
@@ -42,6 +44,39 @@ class Annotations extends React.Component {
     })
   }
 
+  handleMarkerClick(ev, id) {
+    // Make sure we don't trigger any other events (i.e.)
+    ev.stopPropagation()
+
+    // We need to index so that we can
+    const annotationIndex = findIndex(
+      this.state.annotationStore,
+      annotation => annotation.id === id
+    )
+
+    const annotation = this.state.annotationStore[annotationIndex]
+
+    debugger
+
+    // if (annotationIndex === -1)
+
+    // const annotation = get(this.state.annotationStore, annotationIndex)
+
+    // There isn't a corresponding annotation, so we cannot continue
+    if (!annotation) return
+
+    // If the annotation is open, close it, and vice-versa
+    if (annotation.isOpen) {
+      annotation.isOpen = false
+    } else {
+      annotation.isOpen = true
+    }
+
+    this.setState({
+      anno,
+    })
+  }
+
   render() {
     const { annotationStore } = this.state
 
@@ -49,7 +84,11 @@ class Annotations extends React.Component {
       <Container onClick={this.handleSelection}>
         {annotationStore.map(({ x, y, isOpen, id, content }) => (
           <React.Fragment key={`marker-${id}`}>
-            <Marker x={x} y={y} />
+            <Marker
+              x={x}
+              y={y}
+              onClick={ev => this.handleMarkerClick(ev, id)}
+            />
             {isOpen && (
               <Tooltip x={x} y={y}>
                 {content}
