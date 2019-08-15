ffb4dd18cb8755f899549b392a574f74b1a1627d
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-15 22:17:47 +0300

    Adding basic implementation of markers and open tooltips

--- src/annotations.js
+++ src/annotations.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Marker from './marker'
+import Tooltip from './tooltip'
+
 // do your magic here 👇
+// 😎
 
 const Container = styled.div`
   width: 100vw;
@@ -9,13 +13,53 @@ const Container = styled.div`
   background: palegreen;
 `
 
-const handleSelection = ev => {
-  const { pageX: x, pageY: y } = ev
-  alert(`(${x}, ${y})`)
-}
+class Annotations extends React.Component {
+  constructor(props) {
+    super(props)
 
-const Annotations = () => (
-  <Container onClick={handleSelection}>It works</Container>
-)
+    this.state = {
+      annotationStore: [],
+      currentId: 0,
+    }
+
+    this.handleSelection = this.handleSelection.bind(this)
+  }
+
+  handleSelection(ev) {
+    const { pageX: x, pageY: y } = ev
+
+    this.setState({
+      annotationStore: [
+        ...this.state.annotationStore,
+        {
+          x,
+          y,
+          isOpen: true,
+          id: this.state.currentId++,
+          content: 'Some string',
+        },
+      ],
+    })
+  }
+
+  render() {
+    const { annotationStore } = this.state
+
+    return (
+      <Container onClick={this.handleSelection}>
+        {annotationStore.map(({ x, y, isOpen, id, content }) => (
+          <React.Fragment key={`marker-${id}`}>
+            <Marker x={x} y={y} />
+            {isOpen && (
+              <Tooltip x={x} y={y}>
+                {content}
+              </Tooltip>
+            )}
+          </React.Fragment>
+        ))}
+      </Container>
+    )
+  }
+}
 
 export default Annotations
