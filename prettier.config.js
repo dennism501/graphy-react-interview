c6557e8a7afdcd78ee95d352c079f0b98da6f49d
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-15 20:40:50 +0300

    Adding Prettier and config for formatting

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  useTabs: false,
+  printWidth: 80,
+  jsxBracketSameLine: false,
+  parser: 'babel',
+}
