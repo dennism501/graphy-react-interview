b82b44d83bb940999017ffb5a3876ea184ec38ef
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-17 22:48:51 +0300

    Improved visual of the marker

--- src/marker.js
+++ src/marker.js
@@ -9,11 +9,15 @@ const Marker = styled.div`
   height: ${MARKER_SIZE}px;
   border-radius: ${MARKER_SIZE}px;
   border: ${MARKER_BORDER_SIZE}px solid #000;
+  transition: box-shadow 200ms ease 0s, transform 200ms ease 0s;
+  transform: scale(1);
 
   &:hover {
     /* We want the marker to appear above other markers if a user hovers over it */
     cursor: pointer;
     z-index: 1;
+    transform: scale(1.25);
+    box-shadow: rgba(0, 0, 0, 0.133) 0px 0px 2em;
   }
 `
 
