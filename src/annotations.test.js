d49046d311443b4bd80a286f2a8565f3d8b7a880
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-18 01:03:35 +0300

    Starting to write test (now passes ESLint)

--- src/annotations.test.js
+++ src/annotations.test.js
@@ -6,7 +6,14 @@ import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
 
 describe('Annotations', () => {
-  it('should create an annotation when the container is clicked', () => {})
+  it('should create an annotation when the container is clicked', () => {
+    const container = render(<Annotations />)
+
+    fireEvent.click(container, {
+      pageX: 100,
+      pageY: 100,
+    })
+  })
   it('should show the tooltip upon hover of a marker', () => {})
   it('delete an annotation when the delete button is clicked', () => {})
   it('edit an annotation', () => {})
