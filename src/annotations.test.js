fbbb36f3e3ea0832ca253f696bcb3d95d2f83995
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-18 00:43:24 +0300

    Adding annotations base tests

---
+++ src/annotations.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Annotations from './annotations'
+
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+
+describe('Annotations', () => {
+  it('should create an annotation when the container is clicked', () => {})
+  it('should show the tooltip upon hover of a marker', () => {})
+  it('delete an annotation when the delete button is clicked', () => {})
+  it('edit an annotation', () => {})
+  // it('When the annotation should/shouldn't close', () => {})
+})
