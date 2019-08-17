62b37a071469e066dd15fd09ba5d105fef6230af
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-17 17:19:01 +0300

    Improved sizing of markers

--- src/marker.js
+++ src/marker.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const MARKER_SIZE = 25
+export const MARKER_SIZE = 15
 export const MARKER_BORDER_SIZE = 2
 
 const Marker = styled.div`
@@ -12,11 +12,8 @@ const Marker = styled.div`
   border-radius: ${MARKER_SIZE}px;
   border: ${MARKER_BORDER_SIZE}px solid #000;
 
-  z-index: 1;
-
   &:hover {
     /* We want the marker to appear above other markers if a user hovers over it */
-    z-index: 2;
     cursor: pointer;
   }
 `
