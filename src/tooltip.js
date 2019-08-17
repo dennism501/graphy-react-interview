90059d95f1fb78ff300432eebe79b1188f550a52
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-17 17:29:01 +0300

    Attempting to hide the tooltip from the flow of the DOM when it's hidden

--- src/tooltip.js
+++ src/tooltip.js
@@ -1,36 +1,53 @@
 import styled, { css } from 'styled-components'
 
-import { MARKER_SIZE } from './marker'
+import { MARKER_SIZE, MARKER_BORDER_SIZE } from './marker'
 
 const TOOLTIP_SIZE = 150
+const SPACE_BETWEEN_TOOLTIP_AND_MARKER = 5
 
 const Tooltip = styled.div`
-  position: relative;
   background: white;
   box-shadow: 1px 1px 15px -7px rgba(0, 0, 0, 0.29);
-  border-radius: 2px;
-  padding: 5px;
-  width: ${TOOLTIP_SIZE}px;
-  min-width: ${TOOLTIP_SIZE}px;
-  max-width: ${TOOLTIP_SIZE * 2}px;
-  min-height: ${TOOLTIP_SIZE}px;
-  max-height: ${TOOLTIP_SIZE * 2}px;
-  margin-left: 5px;
-  top: -${MARKER_SIZE}px;
-  left: ${MARKER_SIZE}px;
-  cursor: text;
+  margin-top: -${MARKER_SIZE}px;
+  ${'' /*
+    Moving the tooltip away from the original marker position
+  */}
+  margin-left: ${MARKER_SIZE +
+    MARKER_BORDER_SIZE +
+    SPACE_BETWEEN_TOOLTIP_AND_MARKER}px;
   overflow: hidden;
   text-overflow: ellipsis;
   display: flex;
   flex-direction: column;
+  padding: 5px;
+  border-radius: 2px;
 
+  ${
+    '' /*
+    Animating to height and width to make sure the hidden element doesn't show in the flow of the DOM
+    This means that the "hover" area for opening the annotation will be only the marker
+    Animating opacity to fade in/out
+    Using a "delay" on the animation so the user doesn't see the height/width animating, and it appears to be a fade in/out
+    We cannot use the display property as it is not animatable
+  */
+  }
+  min-height: 0;
+  height: 0;
+  min-width: 0;
   opacity: 0;
-  transition: opacity 0.5s ease-in-out;
+  transition: min-width 0ms 400ms, height 0ms 400ms, min-height 0ms 400ms,
+    opacity 400ms 0ms;
 
   ${props =>
     props.isOpen
       ? css`
           opacity: 1;
+          /* Using min height/width so the user can resize the textarea */
+          min-height: ${TOOLTIP_SIZE}px;
+          min-width: ${TOOLTIP_SIZE}px;
+          height: auto;
+          transition: min-width 0ms 0ms, height 0ms 0ms, min-height 0ms 0ms,
+            opacity 600ms 0ms;
         `
       : ''}
 `
