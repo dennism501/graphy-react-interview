a1383f063a812eb54d9ca9961edccebe82aba764
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-18 02:55:45 +0300

    Adding a test for deleting the annotation

--- src/annotations.test.js
+++ src/annotations.test.js
@@ -52,31 +52,25 @@ describe('Annotations', () => {
     expect(await queryAllByTestId('tooltip')).toHaveLength(1)
   })
 
-  // test('deleting an annotation when the delete button is clicked', async () => {})
+  test('deleting an annotation when the delete button is clicked', async () => {
+    const { queryAllByTestId, getByTestId } = render(<Annotations />)
+
+    fireEvent.click(await getByTestId('container'), {
+      pageX: 100,
+      pageY: 100,
+    })
+
+    fireEvent.click(await getByTestId('marker'))
+
+    expect(await queryAllByTestId('tooltip')).toHaveLength(1)
+
+    fireEvent.click(await getByTestId('delete-annotation'))
+
+    expect(await queryAllByTestId('marker')).toHaveLength(0)
+    expect(await queryAllByTestId('tooltip')).toHaveLength(0)
+  })
   // test('editing an annotation', async () => {})
   // test('closing the annotation when <x>', async () => {})
   // test('opening the annotation when <y>', async () => {})
   // test('annotation remaings open when editing', async () => {})
-
-  // WIP in moving this test to here
-  // test('creating an annotation at the required location', async () => {
-  //   // So that we have an firm/nonchanging input and output (i.e. x = 100, padding = 5, marker size = 25, border = 2)
-  //   // We don't want to exactly duplicate the application logic, as it will make the test brittle
-  //   // See: https://www.freecodecamp.org/news/the-right-way-to-test-react-components-548a4736ab22/
-
-  //   const { getByTestId } = render(<Annotations />)
-
-  //   fireEvent.click(await getByTestId('container'), {
-  //     pageX: 100,
-  //     pageY: 100,
-  //   })
-
-  //   // We probably wouldn't normally test the style attributes of a component
-  //   // But in this case, the positioning of the marker is super important
-  //   // We are testing that the annotation gets positioned correctly relative to the initial click
-  //   const annotation = await getByTestId('annotation')
-  //   expect(annotation).toHaveLength(1)
-  //   expect(annotation).toHaveStyle('top: 42px')
-  //   expect(annotation).toHaveStyle('left: 92px')
-  // })
 })
