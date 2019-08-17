11d27d2efb71de6a1b280a262fbe6edb0170afca
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-17 18:31:54 +0300

    Removed unneeded padding around the annotation

--- src/annotation.js
+++ src/annotation.js
@@ -2,19 +2,14 @@ import styled, { css } from 'styled-components'
 
 import { MARKER_SIZE, MARKER_BORDER_SIZE } from './marker'
 
-const ANNOTATION_SPACING = 5
-
 const TOTAL_MARKER_SIZE = MARKER_SIZE + MARKER_BORDER_SIZE
 const MARKER_CENTRE_POINT = Math.ceil(TOTAL_MARKER_SIZE / 2)
 
 const Annotation = styled.div`
   position: absolute;
   /* Centering the marker position to the initial click. */
-  top: ${props => props.y - MARKER_CENTRE_POINT - ANNOTATION_SPACING}px;
-  left: ${props => props.x - MARKER_CENTRE_POINT - ANNOTATION_SPACING}px;
-
-  /* Adding spacing so there is a small window outside the annotation where it won't hide */
-  padding: ${ANNOTATION_SPACING}px;
+  top: ${props => props.y - MARKER_CENTRE_POINT}px;
+  left: ${props => props.x - MARKER_CENTRE_POINT}px;
 
   display: flex;
   flex-direction: row;
