47eae4abf06eeabcc9703115cfcacb41f5134e23
Scott Warren <2718494+scottwarren@users.noreply.github.com>
2019-08-18 15:19:34 +0300

    Fixing the test for editing an annotation, adds check for the annotation remaining open

--- src/annotations.test.js
+++ src/annotations.test.js
@@ -41,7 +41,9 @@ describe('Annotations', () => {
   })
 
   test('showing the tooltip upon click of a marker', async () => {
-    const { queryAllByTestId, getByTestId } = render(<Annotations />)
+    const { queryAllByTestId, getByTestId, queryByTestId } = render(
+      <Annotations />
+    )
 
     fireEvent.click(await getByTestId('container'), {
       pageX: 100,
@@ -51,6 +53,7 @@ describe('Annotations', () => {
     fireEvent.click(await getByTestId('marker'))
 
     expect(await queryAllByTestId('tooltip')).toHaveLength(1)
+    expect(await queryByTestId('tooltip')).toBeVisible()
   })
 
   test('deleting an annotation when the delete button is clicked', async () => {
@@ -99,29 +102,57 @@ describe('Annotations', () => {
       pageY: 100,
     })
 
+    // Open the annotation
     fireEvent.click(await getByTestId('marker'))
-
     fireEvent.click(await getByTestId('annotation-content-container'))
 
+    // Edit the annotation
     userEvent.type(await getByTestId('annotation-editor'), 'foo bar')
 
     expect(await getByTestId('annotation-editor')).toHaveValue('foo bar')
 
+    // Exit editing and save the annotation (which will close the annotation)
     fireEvent.blur(await getByTestId('annotation-editor'))
-
     fireEvent.click(await getByTestId('save-annotation'))
 
     expect(await queryByTestId('tooltip')).not.toBeVisible()
 
-    fireEvent.click(await getByTestId('marker'))
+    // Hover over the marker to open the annotation that is not in editing mode
+    fireEvent.mouseEnter(await getByTestId('marker'))
 
     expect(await queryByTestId('tooltip')).toBeVisible()
+    expect(await getByTestId('annotation-content')).toHaveTextContent('foo bar')
+  })
 
-    // expect(await getByTestId('annotation-content')).toHaveTextContent('foo bar')
+  test('annotation remaings open when editing', async () => {
+    const { queryAllByTestId, getByTestId } = render(<Annotations />)
+
+    fireEvent.click(await getByTestId('container'), {
+      pageX: 100,
+      pageY: 100,
+    })
+
+    // Open the annotation
+    fireEvent.click(await getByTestId('marker'))
+    fireEvent.click(await getByTestId('annotation-content-container'))
+
+    // Edit the annotation
+    userEvent.type(await getByTestId('annotation-editor'), 'foo bar')
+
+    fireEvent.blur(await getByTestId('annotation-editor'))
+
+    // Click elsewhere
+    fireEvent.click(await getByTestId('container'), {
+      pageX: 5,
+      pageY: 5,
+    })
+
+    // We expect both tooltips to be open, and we expect to have created a new marker with the second click
+    expect(await queryAllByTestId('tooltip')).toHaveLength(2)
+    expect(await queryAllByTestId('marker')).toHaveLength(2)
   })
 
   // test('editing an annotation', async () => {})
   // test('closing the annotation when <x>', async () => {})
   // test('opening the annotation when <y>', async () => {})
-  // test('annotation remaings open when editing', async () => {})
 })
